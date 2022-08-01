1ec4af719d343c05096cc2cdcc0c997dc562f3be
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-01 10:31:35 +0200

    Initial Commit

---
+++ GlobalActor/GlobalActorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalActorApp.swift
+//  GlobalActor
+//
+//  Created by Skorobogatow, Christian on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GlobalActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
